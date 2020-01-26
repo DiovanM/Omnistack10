eb221337c559490774289690101c906d92cb9228
Diovan Marques <diovanmarques@gmail.com>
2020-01-26 20:45:10 -0300

    Finished front-web

--- web/src/App.js
+++ web/src/App.js
@@ -1,77 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
 
 function App() {
+
+  const [devs, setDevs] = useState([]);
+
+  useEffect(()=> {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleSubmit(data){
+    
+    const response = await api.post('/devs', data);
+    
+    setDevs([...devs, response.data]);
+  }
+
   return(
     <div id="app">
       <aside>
       <strong>Cadastrar</strong>
-      <form>
-
-        <div class="input-block">
-        <label htmlFor="github_username">Usuário do GitHub</label>
-        <input name="github_username" id="github_username" required></input>
-        </div>
-
-        <div className="input-block">
-        <label htmlFor="techs">Tecnologias</label>
-        <input name="techs" id="techs" required></input>
-        </div>
-
-        <div className="input-group">
-          <div class="input-block">
-          <label htmlFor="latitude">Latitude</label>
-          <input name="latitude" id="latitude" required></input>
-          </div>
-          <div class="input-block">
-          <label htmlFor="longitude">Longitude</label>
-          <input name="longitude" id="longitude" required></input>
-          </div>
-        </div>
-
-        <button type="submit">Salvar</button>
-
-      </form>
+      <DevForm onSubmit={handleSubmit} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/22122763?s=400&v=4" alt="Github Profile"/>
-              <div className="user-info">
-                <strong>Diovan Marques</strong>
-                <span>C#, Unity</span>
-              </div>
-            </header>
-            <p>Diovan Marques maneiro</p>
-            <a href="https://github.com/DiovanM">Acessar perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/22122763?s=400&v=4" alt="Github Profile"/>
-              <div className="user-info">
-                <strong>Diovan Marques</strong>
-                <span>C#, Unity</span>
-              </div>
-            </header>
-            <p>Diovan Marques maneiro</p>
-            <a href="https://github.com/DiovanM">Acessar perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/22122763?s=400&v=4" alt="Github Profile"/>
-              <div className="user-info">
-                <strong>Diovan Marques</strong>
-                <span>C#, Unity</span>
-              </div>
-            </header>
-            <p>Diovan Marques maneiro</p>
-            <a href="https://github.com/DiovanM">Acessar perfil no GitHub</a>
-          </li>
+          {devs.map(dev => (
+              <DevItem key={dev._id} dev={dev}/>
+          ))}
         </ul>
       </main>
     </div>
